b35c78ef981df09b011a4fbd207516e2f9bd48b7
gingy <ggeorg.social@gmail.com>
2026-09-18 20:56:46 +0300

    refactor: removed jpa stuff & added spring bootstrap class

---
+++ src/main/java/com/ggeorg/BlogflowApplication.java
@@ -0,0 +1,12 @@
+package com.ggeorg;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlogflowApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BlogflowApplication.class, args);
+    }
+}
